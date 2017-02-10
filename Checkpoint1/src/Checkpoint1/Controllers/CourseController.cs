a55bcfeae079d30be4d09312487f88dc0d857e57
Staci Holmes-Segarra <stacihs@hotmail.com>
2017-02-10 15:33:47 -0600

    Creates Course Create and Index views

--- Checkpoint1/src/Checkpoint1/Controllers/CourseController.cs
+++ Checkpoint1/src/Checkpoint1/Controllers/CourseController.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Checkpoint1.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Checkpoint1.Controllers
 {
-    public class CourseController
+    public class CourseController : Controller
     {
+        private ApplicationContext db_context;
+
+        public CourseController(ApplicationContext context)
+        {
+            db_context = context;
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            var course = db_context.Course.Include(s => s.Student);
+
+            return View(course.ToList());
+        }
+
     }
 }
