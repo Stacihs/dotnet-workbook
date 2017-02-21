3ad728abbe012001347bfc32817f34b3208bac1c
Staci Holmes-Segarra <stacihs@hotmail.com>
2017-02-21 10:51:29 -0600

    added Especies-checkpoint 2 project

---
+++ ESpecies/src/ESpecies/Models/Donor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESpecies.Models
+{
+    public class Donor
+    {
+        public int DonorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Donation { get; set; }
+        public int SpeciesId { get; set; }
+
+        public ICollection<Species> Species { get; set; }
+    }
+}
---
+++ ESpecies/src/ESpecies/Models/Species.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ESpecies.Models
+{
+    public class Species
+    {
+        public int SpeciesId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Year { get; set; }
+        public string Description { get; set; }
+        public string DonorId { get; set; }
+        public decimal Donation { get; set; }
+
+        public ICollection<Donor> Donor { get; set; }
+   
+    }
+}
