40ec190e71b5e6c225cfa82514f7949073f70919
Staci Holmes-Segarra <stacihs@hotmail.com>
2017-03-09 09:43:06 -0600

    Added files and school model

---
+++ Checkpoint3/src/StudentFinder/Models/School.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentFinder.Models
+{
+    public class School
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public string State { get; set; }
+        public int Phone { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+        public TimeZoneInfo Zone {get; set; }
+    }
+}
