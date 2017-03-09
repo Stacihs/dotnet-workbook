d36cac6ffc1c60f0278027ee481a8c3d66953f11
Staci Holmes-Segarra <stacihs@hotmail.com>
2017-03-09 09:55:03 -0600

    Added Schedule and Space models

---
+++ Checkpoint3/src/StudentFinder/Models/Schedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentFinder.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set;}
+        public string Period { get; set; }
+
+    }
+}
